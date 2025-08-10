643ee0f37204520878c44a06f2100bcf7127d839
Dev <devbhola1902@gmail.com>
2025-08-11 00:25:20 +0530

    final commit before production

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
-import { connectDB } from "./lib/db.js";
-import { app, server } from "./lib/socket.js";
+
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
 dotenv.config();
+
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-// Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -22,24 +25,18 @@ app.use(
   })
 );
 
-// API routes
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// Static file serving for production
 if (process.env.NODE_ENV === "production") {
-  const staticPath = path.join(__dirname, "dist");
-  app.use(express.static(staticPath));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.join(staticPath, "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-// Start server
-server.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
