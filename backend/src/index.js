bd23b8673b971532d25ec79b389f6add5c8bece2
Dev <devbhola1902@gmail.com>
2025-08-10 23:22:53 +0530

    deploying

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,9 @@ import messageRoutes from './routes/message.route.js';
 import cors from 'cors';
 import { connectDB } from './lib/db.js';
 import {app,server} from './lib/socket.js'
-
+import path from 'path';
 dotenv.config();
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
@@ -20,6 +21,13 @@ app.get('/',(req,res)=>{
 });
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+}
+
 server.listen(process.env.PORT,()=>{   
     console.log(`Server is running on port ${process.env.PORT}`);
     connectDB()
